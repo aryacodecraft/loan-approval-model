1e58259649824a57e9154167b389aaa6ff4af83a
Arya <aryamahajan7979@gmail.com>
2025-08-07 23:35:12 +0530

    added precision score, recall and changed model from DecisionTree to XGBClassifier

--- predictor.py
+++ predictor.py
@@ -1,10 +1,9 @@
-from sklearn.tree import DecisionTreeClassifier
+from xgboost import XGBClassifier
 from sklearn.model_selection import train_test_split
-from sklearn.metrics import accuracy_score
+from sklearn.metrics import accuracy_score, precision_score,recall_score
 from sklearn.preprocessing import LabelEncoder
 import pandas as pd
-import matplotlib.pyplot as plt
-from sklearn.tree import plot_tree
+
 
 df = pd.read_csv('data.csv')
 
@@ -25,13 +24,10 @@ y = df['Loan_Status']
 
 x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=42)
 
-model = DecisionTreeClassifier()
+model = XGBClassifier()
 model.fit(x_train, y_train)
 y_pred = model.predict(x_test)
 accuracy = accuracy_score(y_test, y_pred)
 print(f'Accuracy: {accuracy * 100:.2f}%')
-
-plt.figure(figsize=(12, 8))
-plot_tree(model, feature_names=x.columns, class_names=["No", "Yes"], filled=True)
-plt.title("Loan Approval Decision Tree")
-plt.show()
+print(f'Precision: {precision_score(y_test, y_pred) * 100:.2f}%')
+print(f'Recall: {recall_score(y_test, y_pred) * 100:.2f}%')
